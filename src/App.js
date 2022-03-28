a638dfbcfe80f6bf0eaf1a9870617b101c1550ac
cardize <mustafasamisakarya@gmail.com>
2022-03-28 16:11:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useReducer, useState } from 'react'
+import { reducer } from './reducer'
+
+import './App.css'
+
+const initialState = {
+  data: '',
+  loading: false,
+  error: '',
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { data, loading, error } = state
+
+  const randomPic = () => {
+    dispatch({ type: 'FETCH_START' })
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json([0]))
+      .then((response) => {
+        dispatch({ type: 'FETCH_SUCCESS', payload: response.message })
+      })
+      .catch(() => {
+        dispatch({ type: 'FETCH_ERROR', payload: 'Load error!' })
+      })
+  }
+  return (
+    <div className="App">
+      <button onClick={randomPic} disabled={loading}>
+        Random Pic
+      </button>
+      {data && (
+        <div>
+          <img src={data} alt="random pic" />
+          <h1>{data}</h1>
+        </div>
+      )}
+      {error && <p>{error}</p>}
+    </div>
+  )
+}
+
+export default App
